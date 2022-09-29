3c0869b24de51e75f763898a88c6fcad79418ed4
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2022-09-30 03:38:09 +0700

    intro / work / education

---
+++ client/js/main.js
@@ -0,0 +1,17 @@
+const education = [
+    {
+        degree:`span class="fw-bold">B.Eng.</span> Bachelor of Engineering (Digital Engineering)`,
+        school: `Sirindhorn International Institute of Technology (SIIT), Thammasat University`,
+        year: `2019 - Present (Expected Graduation: 2023)`,
+        gpa: `3.94/4.00`,
+        detail: `Specialized in Application software development. <br/>
+        4-year continuous full scholarship recipient <br/>
+        Achived first rank in academic year 2020.`
+    },{
+        degree:`<span class="fw-bold">High School</span> Science and Mathematics Program`,
+        school: `Princess Chulaborn Science High School, Trang`,
+        year: `2016 - 2019`,
+        gpa: `3.71/4.00`,
+        detail: null
+    }
+];
